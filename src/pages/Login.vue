16f1260e7c240a00286f24c01ca56ee2d732dd8b
Ollan Muza <ammmuza@gmail.com>
2024-09-04 16:49:29 +0100

    Change code to show login on signup

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -1,7 +1,34 @@
-<script>
-import SigninForm from '../components/SigninForm.vue';
+<script setup>
+import { ref } from "vue"
+
+let user = ref("")
+let password = ref("")
+
+const onSubmit = () => {
+    console.log(user.value, password.value)
+}
 </script>
 
 <template>
-    <SigninForm />
+    <div class="w-screen h-screen flex justify-center items-center">
+        <form class="flex flex-col w-44" @submit.prevent="onSubmit">
+            <label class="text-sm text-neutral-500" for="user">Username/Email</label>
+            <input class="px-0.5 py-1 border-black border rounded" type="text" id="user" v-model="user"
+                placeholder="username">
+
+            <label class="text-sm text-neutral-500" for="password">Password</label>
+            <input class="px-0.5 py-1 border-black border rounded" type="password" id="password" v-model="password"
+                placeholder="password">
+
+            <button class="mt-2 bg-blue-100 hover:bg-blue-400 hover:text-white rounded" type="submit">
+                Login
+            </button>
+
+            <router-link class="w-full" to="/register">
+                <button class="mt-2 bg-blue-100 hover:bg-blue-400 hover:text-white rounded w-full" type="button">
+                    Sign Up
+                </button>
+            </router-link>
+        </form>
+    </div>
 </template>
